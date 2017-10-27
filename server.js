ba60e51b0129375e8105eb5a75d40a376f65db52
nehrr <iamkapitaan@gmail.com>
2017-10-27 17:07:32 +0200

    added template management

--- server.js
+++ server.js
@@ -1,50 +1,60 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const bodyParser = require('body-parser');
+
 const port = process.argv[2] || 8080;
 
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'public'));
 
-app.get('/', (request, response) => {
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get(/^\/(index)?$/, (request, response) => {
   console.log(`${request.method} ${request.url}`);
-  response.render(path.join(__dirname, 'public', 'index'))
+  response.render('index');
 });
 
 app.get('/create', (request, response) => {
   console.log(`${request.method} ${request.url}`);
-  response.render(path.join(__dirname, 'public', 'create'));
+  response.render('create');
 });
 
 app.get('/list', (request, response) => {
   console.log(`${request.method} ${request.url}`);
-  response.render(path.join(__dirname, 'public', 'list'))
+  response.render('list');
 });
 
 app.get('/update/:id', (request, response) => {
+  console.log(`${request.params}`);
   console.log(`${request.method} ${request.url}`);
-  response.render(path.join(__dirname, 'public', `update`))
+  response.render(`update/${request.params}`);
 });
 
 app.get('*', (request, response) => {
   console.log(`The URL ${request.url} doesn't exist`);
-  response.render(path.join(__dirname, 'public', '404'))
+  response.render('404');
 });
 
 app.post('/add-user', (request, response) => {
+  console.log(request.body);
   console.log(`${request.method} ${request.url}`);
+  response.end();
 });
 
 app.put('/update-user/:id', (request, response) => {
   console.log(`${request.method} ${request.url}`);
+  response.end();
 });
 
 app.delete('/delete-user/:id', (request, response) => {
   console.log(`${request.method} ${request.url}`);
+  response.end();
 });
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
